abcce502d78d5d3e40a6e2ee74a09768b32fb9d1
huaml <huaml@asiainfo.com>
2019-01-02 16:30:06 +0800

    xiaohua_01

---
+++ src/app/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './../image/logo.jpg';
+import './../css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="bg">
+          测试页面
+        </div>
+        <img className="img" src={logo} alt=""/>
+     </div>
+    );
+  }
+}
+
+export default App;
